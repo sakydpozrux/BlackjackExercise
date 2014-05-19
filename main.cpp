e3c74b15a13c05919ea650f09abc3b21ffaa37ea
Szymon Koper <sakydpozrux@gmail.com>
2014-05-19 23:28:47 +0200

    Cleanup in choosing hardcoded input or file input

--- main.cpp
+++ main.cpp
@@ -8,15 +8,26 @@
 #include "card.hpp"
 #include <iostream>
 
+#define USE_HARDCODED_INPUT true
+
 int main(int argc, char* argv[])
 {
-//    if (argc < 2) EXIT_FAILURE;
-//    std::string file_contents = CardListReader::file_contents(std::string(argv[1]));
-//    std::list<Card> cards = CardListReader::create_card_list(file_contents);
+    std::list<Card> cards;
 
-    std::string hardcoded_input = "10 4 10 3 10 2 3 6 9 9 5 1 9 10 10 5 1 8 8 8 10 6 5 6 10 10"
-                                  " 3 10 1 7 4 2 3 7 10 2 7 1 5 10 8 10 7 2 10 10 10 6 9 10 4 4\n";
-    std::list<Card> cards = CardListReader::create_card_list(hardcoded_input);
+    if (USE_HARDCODED_INPUT)
+    {
+        std::string hardcoded_input = "10 4 10 3 10 2 3 6 9 9 5 1 9 10 10 "
+                                      "5 1 8 8 8 10 6 5 6 10 10 3 10 1 7 4 "
+                                      "2 3 7 10 2 7 1 5 10 8 10 7 2 10 10 "
+                                      "10 6 9 10 4 4\n";
+        cards = CardListReader::create_card_list(hardcoded_input);
+    }
+    else
+    {
+        if (argc < 2) EXIT_FAILURE;
+        std::string file_contents = CardListReader::file_contents(std::string(argv[1]));
+        cards = CardListReader::create_card_list(file_contents);
+    }
 
     Deck d(cards);
 
